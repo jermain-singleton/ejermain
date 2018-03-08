0e380e25c375601877727482897924dc85bb71d4
Jermain Singleton <jsingabc@gmail.com>
2018-03-08 12:50:21 -0500

    Add templates, models topic and entries, html pages

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+# imports model to be registered
+from blog.models import Post, Entry
+
+# tell django manage model through admin site and add post on the admin site
+admin.site.register(Post)
+# add a way to add entries on admin site
+admin.site.register(Entry)
+
+
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """A topic a user can post about"""
+    text = models.CharField(max_length=250)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string in a readable format."""
+        return self.text
+
+class Entry(models.Model):
+    """Where to post entries in the topic"""
+    topic = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    # for multiple post instead of Entrys, text will be entries
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+
+    def __str__(self):
+        """return string in readable format for me"""
+        return self.text[:50] + "..." 
